8f84caf29f9d00162fd7dd60cf4553221e760d50
namu5 <code@kakao.com>
2021-03-03 16:51:01 +0900

    token을 mutation의 인자로 넘겨주는 좋지않은 방법

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,12 +1,15 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, userName, email, password: newPassword }
+      { firstName, lastName, userName, email, password: newPassword, token }
     ) => {
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
@@ -14,7 +17,7 @@ export default {
 
       const editUser = await client.user.update({
         where: {
-          id: 1,
+          id,
         },
         data: {
           firstName,
