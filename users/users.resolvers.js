636eb78f5a07ddf82b6e96390eb669176c431e51
imjae <code@kakao.com>
2021-03-14 13:40:41 +0900

    isFollowing computed fields 추가

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -13,5 +13,16 @@ export default {
 
       return id === loggedInUser.id;
     },
+    isFollowing: async ({ id }, _, { loggedInUser }) => {
+    //   const exist = await client.user
+    //     .findUnique({ where: { id: loggedInUser.id } })
+    //     .following({ where: { id } });
+
+      const exists = await client.user.count({
+        where: { userName: loggedInUser.userName, following: { some: { id } } },
+      });
+
+      return Boolean(exists);
+    },
   },
 };
