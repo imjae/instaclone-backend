50a3c2c8652482d6b2f16a1c25ce0bb6158a1345
imjae <code@kakao.com>
2021-03-20 14:03:08 +0900

    uploadPhoto 뮤테이션 껍데기 생성

---
+++ src/photos/photos.typeDefs.ts
@@ -0,0 +1,34 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  type Photo {
+    #         id        Int       @id @default(autoincrement())
+    #   createdAt DateTime  @default(now())
+    #   updatedAt DateTime  @updatedAt
+    #   User      User?     @relation(fields: [userId], references: [id])
+    #   userId    Int?
+    #   hashtags  Hashtag[]
+    #   file      String
+    #   caption   String?
+    id: Int!
+    user: User
+    file: String!
+    caption: String
+    hashtags: [Hashtag]
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Hashtag {
+    #     id        Int      @id @default(autoincrement())
+    #   hashtag   String
+    #   createdAt DateTime @default(now())
+    #   updatedAt DateTime @updatedAt
+    #   photos    Photo[]
+    id: Int!
+    hashtag: String!
+    photos: [Photo]!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
---
+++ src/photos/uploadPhoto/uploadPhoto.resolvers.ts
@@ -0,0 +1,22 @@
+import { Resolvers } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    uploadPhoto: protectedResolver(
+      async (_, { file, caption }, { loggedInUser }) => {
+        if (caption) {
+          // caption 파싱하기
+          
+          // 파싱한 caption의 hashtag가 존재한다면, 기존 hashrag 가져오고,
+          // 아니면 새로 생성한다.
+        }
+        // 파싱된 hashtag과 함께 사진 저장한다.
+
+        // 해당 사진을 hashtags에 추가한다
+      }
+    ),
+  },
+};
+
+export default resolvers;
