8d67cee0c8d3c580fd45608a18395a4337d471e7
imjae <code@kakao.com>
2021-03-21 18:58:05 +0900

    processhashtag 유틸함수 수정

--- src/photos/photos.utils.ts
+++ src/photos/photos.utils.ts
@@ -1,5 +1,3 @@
-import { Prisma } from ".prisma/client";
-
 const processHashtag = (caption: string): any => {
   const hashtags = caption.match(/#[\w]+/g) || [];
   return hashtags.map((hashtag: String) => ({
