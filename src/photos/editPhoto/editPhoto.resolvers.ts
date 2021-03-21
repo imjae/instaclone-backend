3d0f0d2a22f639c03f164c982b48777577d1659b
imjae <code@kakao.com>
2021-03-21 17:35:59 +0900

    editPhoto 기능 추가 , 해쉬태그 업데이트 기능 구현 전 시점

---
+++ src/photos/editPhoto/editPhoto.resolvers.ts
@@ -0,0 +1,38 @@
+import { Resolvers } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    editPhoto: protectedResolver(
+      async (_, { id, caption }, { loggedInUser, client }) => {
+        const ok = await client.photo.findFirst({
+          where: { id, userId: loggedInUser.id },
+        });
+
+        if (!ok) {
+          return {
+            ok: false,
+            error: "Photo not found",
+          };
+        }
+
+        const photo = await client.photo.update({
+          where: {
+            id,
+          },
+          data: {
+            caption,
+          },
+        });
+
+        if (photo) {
+          return {
+            ok: true,
+          };
+        }
+      }
+    ),
+  },
+};
+
+export default resolvers;
