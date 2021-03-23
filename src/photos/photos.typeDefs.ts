9980772014eaee2be109705058891f7d3809307f
imjae <code@kakao.com>
2021-03-23 14:00:01 +0900

    hashtag 모델 totalPhotos non required 추가

--- src/photos/photos.typeDefs.ts
+++ src/photos/photos.typeDefs.ts
@@ -2,34 +2,20 @@ import { gql } from "apollo-server-express";
 
 export default gql`
   type Photo {
-    #         id        Int       @id @default(autoincrement())
-    #   createdAt DateTime  @default(now())
-    #   updatedAt DateTime  @updatedAt
-    #   User      User?     @relation(fields: [userId], references: [id])
-    #   userId    Int?
-    #   hashtags  Hashtag[]
-    #   file      String
-    #   caption   String?
     id: Int!
     user: User!
     file: String!
     caption: String
     hashtags: [Hashtag]
-    likes: [Like]
     createdAt: String!
     updatedAt: String!
   }
 
   type Hashtag {
-    #     id        Int      @id @default(autoincrement())
-    #   hashtag   String
-    #   createdAt DateTime @default(now())
-    #   updatedAt DateTime @updatedAt
-    #   photos    Photo[]
     id: Int!
     hashtag: String!
     photos(page: Int!): [Photo]!
-    totalPhotos: Int!
+    totalPhotos: Int
     createdAt: String!
     updatedAt: String!
   }
