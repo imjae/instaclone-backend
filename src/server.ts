da37af8247feb1f511fb2686c3260f6633242f64
imjae <code@kakao.com>
2021-04-06 18:10:33 +0900

    subscription을위한 설정파일 작성

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
 import client from "./client";
+import pubsub from "./pubsub";
 
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
@@ -20,10 +21,11 @@ const server = new ApolloServer({
 const app = express();
 app.use(logger("tiny"));
 app.use("/statics", express.static("uploads"));
-server.applyMiddleware({ app });
+apollo.applyMiddleware({ app });
+apollo.installSubscriptionHandlers(app);
 
 const PORT = process.env.PORT;
 
-app.listen({port: PORT}, () => {
+app.listen({ port: PORT }, () => {
   console.log(`🚀 Server ready at https://localhost:${PORT}`);
 });
